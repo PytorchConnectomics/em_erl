12a36ccf9e85610fbc8d5af08cb7d92a161ddd6f
donglaiw <weidf@l001.cm.cluster>
2024-10-29 16:13:20 -0400

    add vol_to_skel

--- em_erl/io.py
+++ em_erl/io.py
@@ -239,3 +239,37 @@ def write_h5(filename, data, dataset="main"):
         )
         ds[:] = data
     fid.close()
+
+
+def vol_to_skel(labels, scale=1.5, const=500, obj_ids=None, dust_size = 100, res = (32,32,30), num_thread = 1):
+    # res: xyz
+    if obj_ids is None:
+        obj_ids = np.unique(labels)
+        obj_ids = obj_ids[obj_ids>0]
+    obj_ids = list(obj_ids)
+    skels = kimimaro.skeletonize(
+      labels,
+      teasar_params={
+        'scale': scale,
+        'const': const, # physical units
+        'pdrf_exponent': 4,
+        'pdrf_scale': 100000,
+        'soma_detection_threshold': 1100, # physical units
+        'soma_acceptance_threshold': 3500, # physical units
+        'soma_invalidation_scale': 1.0,
+        'soma_invalidation_const': 300, # physical units
+        'max_paths': 50, # default  None
+      },
+      object_ids= obj_ids, # process only the specified labels
+      # object_ids=[ ... ], # process only the specified labels
+      # extra_targets_before=[ (27,33,100), (44,45,46) ], # target points in voxels
+      # extra_targets_after=[ (27,33,100), (44,45,46) ], # target points in voxels
+      dust_threshold = dust_size, # skip connected components with fewer than this many voxels
+      anisotropy = res, # default True
+      fix_branching=True, # default True
+      fix_borders=True, # default True
+      progress=True, # default False, show progress bar
+      parallel = num_thread, # <= 0 all cpu, 1 single process, 2+ multiprocess
+      parallel_chunk_size = 100, # how many skeletons to process before updating progress bar
+    )
+    return skels
